25231b6e38cd5df69466ecf70e32adf2ce8930de
Benedikt Breitschopf <benedikt.breitschopf@sap.com>
2015-02-19 13:31:03 +0100

    get hint update
    Change-Id: I90f1aef1e54c1d890f0748f1e2a0a4439521861e

--- gethint.php
+++ gethint.php
@@ -1,35 +1,13 @@
 <?php
 // Array with names
-$a[] = "Anna";
-$a[] = "Brittany";
-$a[] = "Cinderella";
-$a[] = "Diana";
-$a[] = "Eva";
-$a[] = "Fiona";
-$a[] = "Gunda";
-$a[] = "Hege";
-$a[] = "Inga";
-$a[] = "Johanna";
-$a[] = "Kitty";
-$a[] = "Linda";
-$a[] = "Nina";
-$a[] = "Ophelia";
-$a[] = "Petunia";
-$a[] = "Amanda";
-$a[] = "Raquel";
-$a[] = "Cindy";
-$a[] = "Doris";
-$a[] = "Eve";
-$a[] = "Evita";
-$a[] = "Sunniva";
-$a[] = "Tove";
-$a[] = "Unni";
-$a[] = "Violet";
-$a[] = "Liza";
-$a[] = "Elizabeth";
-$a[] = "Ellen";
-$a[] = "Wenche";
-$a[] = "Vicky";
+$a[] = "Anfrage";
+$a[] = "Nachhilfe";
+$a[] = "Aufsatz";
+$a[] = "Rechtschreibung";
+$a[] = "Lese-Rechtschreib-Schwäche";
+$a[] = "Aufsatz";
+$a[] = "Dyskalkulie";
+$a[] = "Rechenschwäche";
 
 // get the q parameter from URL
 $q = $_REQUEST["q"];
