91bf6d6032aa68f302d3ae5a030566a12cca122d
Benedikt Breitschopf <benedikt.breitschopf@gmail.com>
2015-03-14 16:23:56 +0100

    login updated
    Change-Id: I044f22bdae7ab6cf4d74eea7187c752d9ab89876

--- WebContent/geheim.php
+++ WebContent/geheim.php
@@ -5,7 +5,7 @@ session_start();
 <?php 
 if(!isset($_SESSION["username"])) 
    { 
-   echo "Bitte erst <a href=\"loginNEW.html\">einloggen</a>"; 
+   echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
    exit; 
    }    
 ?> 
--- WebContent/login.php
+++ WebContent/login.php
@@ -1,105 +1,28 @@
 <?php
-
-//Cookie
-setcookie("username","valentin",time()+(3600*24));
-$cookie = $_COOKIE["username"];
-echo "Der Inhalt des Cookies: $cookie";
-
-
-$_db_host = "localhost"; // meist localhost
-$_db_datenbank = "homepage";
-$_db_username = "root";
-$_db_password = "root";
-
-SESSION_START ();
-
-// Datenbankverbindung herstellen
-$link = mysql_connect ( $_db_host, $_db_username, $_db_password );
-
-// Hat die Verbindung geklappt?
-if (! $link) {
-	die ( "Keine Datenbankverbindung möglich: " . mysql_error () );
-}
-
-// Verbindung zur richtigen Datenbank herstellen
-$datenbank = mysql_select_db ( $_db_datenbank, $link );
-
-if (! $datenbank) {
-	echo "Kann die Datenbank nicht benutzen: " . mysql_error ();
-	mysql_close ( $link ); // Datenbank schliessen
-	exit (); // Programm beenden !
-}
-
-// #################################################################
-
-// Ist die $_POST Variable submit nicht leer ???
-// dann wurden Logindaten eingegeben, die müssen wir überprüfen !
-if (! empty ( $_POST ["submit"] )) {
-	// Die Werte die im Loginformular eingegeben wurden "escapen",
-	// damit keine Hackangriffe über den Login erfolgen können !
-	// Mysql_real_escape ist auf jedenfall dem Befehle addslashes()
-	// vorzuziehen !!! Ohne sind mysql injections möglich !!!!
-	$_username = mysql_real_escape_string ( $_POST ["username"] );
-	$_passwort = mysql_real_escape_string ( $_POST ["password"] );
-	
-	// Befehl für die MySQL Datenbank
-	$_sql = "SELECT * FROM login_user WHERE
-                    username='$_username' AND
-                    password='$_password' AND
-                    user_geloescht=0
-                LIMIT 1";
-	
-	// Prüfen, ob der User in der Datenbank existiert !
-	try {
-		$_res = mysql_query ( $_sql, $link );
-	} catch ( Exception $e ) {
-		echo "query klappt nicht";
-	}
-	;
-	try {
-		$_anzahl = @mysql_num_rows ( $_res );
-	} catch ( Exception $e ) {
-		echo "anzahlt wird nich erhöht";
-	}
-	
-	// Die Anzahl der gefundenen Einträge überprüfen. Maximal
-	// wird 1 Eintrag rausgefiltert (LIMIT 1). Wenn 0 Einträge
-	// gefunden wurden, dann gibt es keinen Usereintrag, der
-	// gültig ist. Keinen wo der Username und das Passwort stimmt
-	// und user_geloescht auch gleich 0 ist !
-	if ($_anzahl > 0) {
-		echo "Der Login war erfolgreich.<br>";
-		
-		// In der Session merken, dass der User eingeloggt ist !
-		$_SESSION ["login"] = 1;
-		
-		// Den Eintrag vom User in der Session speichern !
-		$_SESSION ["user"] = mysql_fetch_array ( $_res, MYSQL_ASSOC );
-		
-		// Das Einlogdatum in der Tabelle setzen !
-		$_sql = "UPDATE login_user SET letzter_login=NOW()
-                     WHERE id=" . $_SESSION ["user"] ["id"];
-		mysql_query ( $_sql );
-	} else {
-		echo "Die Logindaten sind nicht korrekt.<br>";
-	}
-}
-
-// Ist der User eingeloggt ???
-if ($_SESSION ["login"] == 0) {
-	// ist nicht eingeloggt, also Formular anzeigen, die Datenbank
-	// schliessen und das Programm beenden
-	include ("login-formular.html");
-	mysql_close ( $link );
-	exit ();
-}
-
-// Hier wäre der User jetzt gültig angemeldet ! Hier kann
-// Programmcode stehen, den nur eingeloggte User sehen sollen !!
-echo "Hallo, Sie sind erfolgreich eingeloggt !<br>";
-
-// #################################################################
-
-// Datenbank wieder schliessen
-mysql_close ( $link );
+session_start();
 ?>
+
+<?php 
+$verbindung = mysql_connect("localhost", "root" , "") 
+or die("Verbindung zur Datenbank konnte nicht hergestellt werden"); 
+mysql_select_db("homepage") or die ("Datenbank konnte nicht ausgewählt werden"); 
+
+$username = $_POST["username"]; 
+$passwort = md5($_POST["password"]); 
+
+$abfrage = "SELECT username, passwort FROM login WHERE username LIKE '$username' LIMIT 1"; 
+$ergebnis = mysql_query($abfrage); 
+$row = mysql_fetch_object($ergebnis); 
+
+if($row->passwort == $passwort) 
+    { 
+    $_SESSION["username"] = $username; 
+    echo "Login erfolgreich. <br> <a href=\"geheim.php\">Geschützer Bereich</a>"; 
+    } 
+else 
+    { 
+    echo "Benutzername und/oder Passwort waren falsch. <a href=\"login.html\">Login</a>"; 
+    } 
+
+?>
+
