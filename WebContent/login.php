d881b80a9e6a80cbb5690096faf1f80e3b21d009
D060569 <D060569@134.155.241.81>
2015-03-15 12:30:55 +0100

    Cookie Update
    Change-Id: I3690395d07f7d17fc8ef335665f9229917f7382e

--- WebContent/login.php
+++ WebContent/login.php
@@ -10,10 +10,10 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1
 
 <?php
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "Cookie named '" . $cookie_name . "' is not set!";
+    echo "Cookie '" . $cookie_name . "' wurde zuvor noch nicht gesetzt!";
 } else {
-    echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo "Value is: " . $_COOKIE[$cookie_name];
+    echo "Cookie '" . $cookie_name . "' wurde bereits gesetzt!<br>";
+    echo "Willkommen zurück " . $_COOKIE[$cookie_name];
 }
 ?>
 
